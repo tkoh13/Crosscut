9e5327f5d1f53fa8d07bc594492138e94e0e4308
tkoh13 <tylerkoh93@gmail.com>
2022-01-06 23:47:37 -0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import Grid from "./scripts/grid"
-
+import Grid from "./scripts/grid";
+import Graph from "./scripts/Graph";
 
 document.addEventListener("DOMContentLoaded", () => {
-    let height = Math.floor(document.documentElement.clientHeight * .75);
-    let width = Math.floor(document.documentElement.clientWidth * .9);
-    let grid = new Grid(height, width)
-    // let nodesArr = grid.nodesArr
-    grid.renderGrid(document.getElementById("table_grid"));
-    // console.log(grid)
-    grid.addListenerForControls();
-    grid.addListenerForAddingWalls();
-    grid.addListenerForTutorial();
-
-})
-
+  // let height = Math.floor(document.documentElement.clientHeight * .2);
+  let height = Math.floor(document.documentElement.clientHeight * 0.6);
+  // let width = Math.floor(document.documentElement.clientWidth * .3);
+  let width = Math.floor(document.documentElement.clientWidth * 0.9);
+  let graph = new Graph(height, width);
+  graph.setup();
+  // console.log(graph)
+  // let grid = new Grid(height, width)
+  // let nodesArr = grid.nodesArr
+  // grid.renderGraph(document.getElementById("table_grid"));
+  // grid.renderGrid(document.getElementById("table_grid"));
+  // console.log(grid)
+  // grid.addListenerForControls();
+  // grid.addListenerForChangingNodes();
+  // grid.addListenerForAddingWalls();
+  // grid.addListenerForTutorial();
+});
